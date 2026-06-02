2202e23e404d7c4f6fe1f4f9304f702b39bcb365
Hyounwook Chang <joseph.hw.chang@gmail.com>
2026-06-03 01:48:37 +0900

    daily

---
+++ prob-#3633/solution_c++.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
+        int n = landStartTime.size();
+        int m = waterStartTime.size();
+
+        int end = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int land = landStartTime[i] + landDuration[i];
+                int land_water = max(land, waterStartTime[j]) + waterDuration[j];
+                end = min(end, land_water);
+
+                int water = waterStartTime[j] + waterDuration[j];
+                int water_land = max(water, landStartTime[i]) + landDuration[i];
+                end = min(end, water_land);
+            }
+        }
+        return end;
+    }
+};
